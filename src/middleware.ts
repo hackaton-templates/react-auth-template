aa79f4795654c55fa701aa3441ed0cb34ae7f975
StarPandaBeg <kshmurakov@mail.ru>
2024-05-21 23:37:03 +0300

    feat: add /signup page

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!signup|_next/static|_next/image|favicon.ico).*)",
   ],
 };
 
