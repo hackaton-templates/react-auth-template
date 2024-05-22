aed094fd3e22dfe22f98c87f48578fe7781d5a3e
StarPandaBeg <kshmurakov@mail.ru>
2024-05-22 14:16:46 +0300

    feat: add CookieMixedHandler
    this handler encapsulates different server & client logic and provides cookies on both of them
    - on server, it will use next/headers
    - on client, it will use cookies-next

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -65,3 +65,61 @@ export class CookieResponseHandler implements CookieHandler {
     return this.response.cookies.has(key);
   }
 }
+
+export class CookieMixedHandler implements CookieHandler {
+  private isServer: boolean = false;
+  private handler: ReturnType<NextCookies> | JsCookies | undefined;
+
+  constructor() {
+    if (typeof window === "undefined") {
+      this.isServer = true;
+    }
+  }
+
+  async set(key: string, value: string) {
+    const handler = await this._getHandler();
+    if (this.isServer) {
+      (handler as ReturnType<NextCookies>).set(key, value);
+    }
+    (handler as JsCookies).setCookie(key, value);
+  }
+
+  async get(key: string, fallback: string | undefined = undefined) {
+    if (!(await this.has(key))) return fallback;
+
+    const handler = await this._getHandler();
+    if (this.isServer) {
+      return (handler as ReturnType<NextCookies>).get(key)!.value;
+    }
+    return (handler as JsCookies).getCookie(key);
+  }
+
+  async delete(key: string) {
+    const handler = await this._getHandler();
+    if (this.isServer) {
+      (handler as ReturnType<NextCookies>).delete(key);
+    }
+    (handler as JsCookies).deleteCookie(key);
+  }
+
+  async has(key: string) {
+    const handler = await this._getHandler();
+    if (this.isServer) {
+      return (handler as ReturnType<NextCookies>).has(key);
+    }
+    return (handler as JsCookies).hasCookie(key);
+  }
+
+  private async _getHandler() {
+    if (this.handler !== undefined) return this.handler;
+
+    if (typeof window === "undefined") {
+      const handler = await import("next/headers");
+      this.handler = handler.cookies();
+    } else {
+      const handler = await import("cookies-next");
+      this.handler = handler;
+    }
+    return this.handler;
+  }
+}
