81d2f60d876bf9b118dfeca00b402a8c44f77c85
StarPandaBeg <kshmurakov@mail.ru>
2024-05-22 14:45:34 +0300

    feat: add sign-up logic

--- src/components/auth/sign-up-form.tsx
+++ src/components/auth/sign-up-form.tsx
@@ -24,17 +24,57 @@ import signUpForm from "@/lib/forms/sign-up.form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
+import useRepository from "@/hooks/repository";
+import authService from "@/services/auth.service";
+import authStorageService from "@/services/auth-storage.service";
+import userStorageService from "@/services/user-storage.service";
+import { useToast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
 
 export default function SignUpForm() {
   const form = useForm<z.infer<typeof signUpForm>>({
     resolver: zodResolver(signUpForm),
     defaultValues: {
-      username: "",
+      email: "",
+      name: "",
       password: "",
+      confirm: "",
     },
   });
+  const { api } = useRepository();
+  const { toast } = useToast();
+  const router = useRouter();
 
-  const handleSubmit: SubmitHandler<SignUpDto> = async (data) => {};
+  const handleSubmit: SubmitHandler<SignUpDto> = async (data) => {
+    try {
+      const newUser = (await api.users.create(data)).data;
+
+      const authResult = await authService.signIn(data);
+      authStorageService.set(authResult);
+
+      const user = await authService.getMe(authResult);
+      userStorageService.set(user);
+
+      toast({
+        title: "Добро пожаловать!",
+        description: `Мы рады тебя видеть, ${user.name}!`,
+      });
+      router.push("/");
+    } catch (e) {
+      // @ts-ignore
+      if (e.response?.status == 409) {
+        form.setError("email", { message: "Email уже используется!" });
+        return;
+      }
+
+      console.error(e);
+      toast({
+        variant: "destructive",
+        title: "Ошибка",
+        description: "При регистрации произошла неизвестная ошибка",
+      });
+    }
+  };
 
   return (
     <Card className="min-w-96">
